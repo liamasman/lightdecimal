a0303fc1ce43fdc54e9189e7132c2874ff961062
Liam Asman <liam@liamasman.com>
2025-03-14 17:00:38 +0000

    More tests for String conversion

--- src/test/java/com/liamasman/lightarithmetic/LightDecimalTest.java
+++ src/test/java/com/liamasman/lightarithmetic/LightDecimalTest.java
@@ -50,5 +50,38 @@ class LightDecimalTest {
         assertEquals("9007199254740992.5", new LightDecimal("9007199254740992.5").toString());
         assertEquals("0.33333333333333333333", new LightDecimal("0.33333333333333333333").toString());
         assertEquals("123456789012345.12345678901234567891", new LightDecimal("123456789012345.12345678901234567891").toString());
+        assertEquals("1.00", new LightDecimal("1.00").toString());
+        assertEquals("1.10", new LightDecimal("1.10").toString());
+        assertEquals("10.00", new LightDecimal("10.00").toString());
+        assertEquals("1", new LightDecimal("01").toString());
+        assertEquals("1.0", new LightDecimal("01.0").toString());
+        assertEquals("0.00", new LightDecimal("00.00").toString());
+        assertEquals("0", new LightDecimal("0000").toString());
+        assertEquals("0.00005300", new LightDecimal("00.00005300").toString());
+        assertEquals("500000", new LightDecimal("500000").toString());
+        assertEquals("500000.0", new LightDecimal("500000.0").toString());
+        assertEquals("512", new LightDecimal("00512").toString());
+        assertEquals("512.0", new LightDecimal("00512.0").toString());
+        assertEquals("42.635400", new LightDecimal("0000042.635400").toString());
+        assertEquals("12000", new LightDecimal("000012000").toString());
+        assertEquals("12000.00", new LightDecimal("000012000.00").toString());
+        assertEquals("12000.0650", new LightDecimal("000012000.0650").toString());
+        assertEquals("-1.00", new LightDecimal("-1.00").toString());
+        assertEquals("-1.10", new LightDecimal("-1.10").toString());
+        assertEquals("-10.00", new LightDecimal("-10.00").toString());
+        assertEquals("-1", new LightDecimal("-01").toString());
+        assertEquals("-1.0", new LightDecimal("-01.0").toString());
+        assertEquals("0.00", new LightDecimal("-00.00").toString());
+        assertEquals("0.00", new LightDecimal("-0.00").toString());
+        assertEquals("0", new LightDecimal("-0").toString());
+        assertEquals("-0.00005300", new LightDecimal("-00.00005300").toString());
+        assertEquals("-500000", new LightDecimal("-500000").toString());
+        assertEquals("-500000.0", new LightDecimal("-500000.0").toString());
+        assertEquals("-512", new LightDecimal("-00512").toString());
+        assertEquals("-512.0", new LightDecimal("-00512.0").toString());
+        assertEquals("-42.635400", new LightDecimal("-0000042.635400").toString());
+        assertEquals("-12000", new LightDecimal("-000012000").toString());
+        assertEquals("-12000.00", new LightDecimal("-000012000.00").toString());
+        assertEquals("-12000.0650", new LightDecimal("-000012000.0650").toString());
     }
 }
